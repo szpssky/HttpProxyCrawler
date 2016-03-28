634283d3193f3f78038276e106d535fa4d7d9b5d
ZhipengShen <szpssky@gmail.com>
2016-03-28 15:33:23 +0800

    修改参数

--- CheckAvailableDaemon.py
+++ CheckAvailableDaemon.py
@@ -41,7 +41,7 @@ def check():
                 'Connection': 'keep-alive',
                 'Host': 'www.baidu.com',
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
-            }, proxies=proxy_para, timeout=1)
+            }, proxies=proxy_para, timeout=2)
             with open('test.html') as f:
                 f.write(response.txt)
             list_addr.append(proxy_para['http'])
